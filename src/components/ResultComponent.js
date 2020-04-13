af42c6e7fcba63273681b86dd486e3199b8db998
Azhagu <azhaguavudainayaki@gmail.com>
2020-04-13 14:07:18 +0530

    init

--- src/components/ResultComponent.js
+++ src/components/ResultComponent.js
@@ -6,8 +6,8 @@ class ResultComponent extends Component {
     render() {
         let {result} = this.props;
         return (
-            <div className="result">
-                <p>{result}</p>
+           <div className="result">
+                 <p>{result}</p>
             </div>
     )
         ;
